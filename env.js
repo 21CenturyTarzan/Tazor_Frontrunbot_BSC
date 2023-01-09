2d0a96e9f9c90b2710fd3275d229714da20d8791
speed <speedjudy928@gmail.com>
2023-01-09 05:26:32 +0300

    this is frontrunning bot on bsc

---
+++ env.js
@@ -0,0 +1,14 @@
+const PR_K = "your private key";
+
+const WBNB_ADDRESS = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
+const TOKEN_ADDRESS = '0x0173295183685f27c84db046b5f0bea3e683c24b'; // CAT token address on BSC
+const AMOUNT = 0.02
+const LEVEL = 0.05
+
+module.exports = {
+    PR_K,
+    TOKEN_ADDRESS,
+    WBNB_ADDRESS,
+    AMOUNT,
+    LEVEL
+};
